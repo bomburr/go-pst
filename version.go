fd3e3e91eeb3c8d00fc4e3d031ecac0ac74d6e8f
mooijtech <info@mooijtech.com>
2020-05-26 19:18:40 +0200

    Add versioning

---
+++ version.go
@@ -0,0 +1,6 @@
+// This file is part of go-pst (https://github.com/mooijtech/go-pst)
+// Copyright (C) 2020 Marten Mooij (https://www.mooijtech.com/)
+package main
+
+// Keeps track of the current version of go-pst.
+const version = "0.0.1"
